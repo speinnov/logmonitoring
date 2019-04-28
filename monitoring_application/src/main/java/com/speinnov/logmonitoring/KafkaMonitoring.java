e9901426397de34cf4712fb931af814ce6adc44f
khenouche abdelhak <akhenouche@spectrum-innovation.com>
2019-04-29 01:43:37 +0200

    slf4j with KAFKA

--- monitoring_application/src/main/java/com/speinnov/logmonitoring/KafkaMonitoring.java
+++ monitoring_application/src/main/java/com/speinnov/logmonitoring/KafkaMonitoring.java
@@ -1,8 +1,10 @@
 package com.speinnov.logmonitoring;
 
 import org.apache.logging.log4j.LogManager;
+
 import org.apache.logging.log4j.Logger;
 
+/**
 public class KafkaMonitoring {
 	private static final Logger LOGGER = LogManager.getLogger(sl4jMonitoring.class.getName());
     
@@ -11,6 +13,60 @@ public class KafkaMonitoring {
     
 	 public String Sample()
 	    {
+		 
+		 Properties props = new Properties();
+		 props.put("bootstrap.servers", "broker1:9092,broker2:9092");
+		 props.put("group.id", "CountryCounter");
+		 props.put("key.deserializer",
+		     "org.apache.kafka.common.serialization.StringDeserializer");
+		 props.put("value.deserializer",
+		     "org.apache.kafka.common.serialization.StringDeserializer");
+
+		 KafkaConsumer<String, String> consumer =  new KafkaConsumer<String, String>(props);
+		 
+		 consumer.subscribe(Collections.singletonList("customerCountries")); 
+		 consumer.subscribe(Pattern.compile("test.*"));
+		 
+		 
+		 try {
+			    while (true) { 1
+			        ConsumerRecords<String, String> records = consumer.poll(100); 2
+			        for (ConsumerRecord<String, String> record : records) 3
+			        {
+			            log.debug("topic = %s, partition = %d, offset = %d,"
+			                customer = %s, country = %s\n",
+			                record.topic(), record.partition(), record.offset(),
+			                record.key(), record.value());
+
+			            int updatedCount = 1;
+			            if (custCountryMap.countainsValue(record.value())) {
+			                updatedCount = custCountryMap.get(record.value()) + 1;
+			            }
+			            custCountryMap.put(record.value(), updatedCount)
+
+			            JSONObject json = new JSONObject(custCountryMap);
+			            System.out.println(json.toString(4)) 4
+			        }
+			    }
+			} finally {
+			    consumer.close(); 5
+			}
+		 
+		 while (true) {
+			    ConsumerRecords<String, String> records = consumer.poll(100);
+			    for (ConsumerRecord<String, String> record : records) {
+			        System.out.printf("topic = %s, partition = %s, offset =
+			            %d, customer = %s, country = %s\n",
+			            record.topic(), record.partition(),
+			            record.offset(), record.key(), record.value()); 1
+			    }
+			    try {
+			        consumer.commitSync(); 2
+			    } catch (CommitFailedException e) {
+			        log.error("commit failed", e) 3
+			    }
+			}
+		 
 		 System.out.println("Kafka monitoring");
 	        LOGGER.debug("Debug Message Logged !!");
 	        LOGGER.info("Info Message Logged !!");
@@ -20,3 +76,4 @@ public class KafkaMonitoring {
 
 
 }
+**/
---
+++ monitoring_application/src/main/java/com/speinnov/logmonitoring/LoggerRoot.java
@@ -0,0 +1,19 @@
+package com.speinnov.logmonitoring;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+ 
+public class LoggerRoot {
+ 
+    private static final Logger logger = LoggerFactory.getLogger(LoggerRoot.class.getSimpleName());
+ 
+    public static void main(String... args) throws InterruptedException {
+        int counter = 0;
+        while (counter < 10) {
+            logger.info("Counter:" + counter);
+            counter++;
+ 
+        }
+        //Thread.sleep(S=1000);
+        logger.info("Completed");
+    }
+}
