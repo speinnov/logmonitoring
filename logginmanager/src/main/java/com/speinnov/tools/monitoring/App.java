dcdd292cbb07516dd270f05a5bc7975da8b20f8b
khenouche abdelhak <akhenouche@spectrum-innovation.com>
2019-05-01 14:05:59 +0200

    import new project to deploy on nexus

---
+++ logginmanager/src/main/java/com/speinnov/tools/monitoring/App.java
@@ -0,0 +1,13 @@
+package com.speinnov.tools.monitoring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ logginmanager/src/test/java/com/speinnov/tools/monitoring/AppTest.java
@@ -0,0 +1,20 @@
+package com.speinnov.tools.monitoring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
