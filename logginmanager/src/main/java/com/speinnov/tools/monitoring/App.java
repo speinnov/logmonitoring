571ccd39ca3f22df6ce0363eac930e5a8cb8ecd0
khenouche abdelhak <akhenouche@spectrum-innovation.com>
2019-05-01 17:45:24 +0200

    updat loggingmanager

--- logginmanager/src/main/java/com/speinnov/tools/monitoring/App.java
+++ logginmanager/src/main/java/com/speinnov/tools/monitoring/App.java
@@ -1,13 +1,23 @@
 package com.speinnov.tools.monitoring;
 
+//import org.slf4j.impl.SimpleLogger;
+
+//import org.slf4j.impl.SimpleLoggerFactory;
+//apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	//private static final Logger LOGGER = LogManager.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!"+App.class.getName() );
+      //  LOGGER.debug("Debug Message Logged !!"+App.class.getName() );
+      //  LOGGER.info("Info Message Logged !!"+App.class.getName() );
+      //  LOGGER.error("Another Debug Message !!"+App.class.getName() );
     }
 }
