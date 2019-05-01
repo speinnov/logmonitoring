c75d559e8abdaf49b2f98641a8dcbc3b3babca1e
khenouche abdelhak <akhenouche@spectrum-innovation.com>
2019-05-01 18:20:53 +0200

    create project tools for logging

--- logginmanager/init.with.git.sh
+++ logginmanager/init.with.git.sh
@@ -1,6 +1,6 @@
 git init
 git add README.md
-got commit -m "create project tools for logging"
+git commit -m "create project tools for logging"
 git rmote add origin https://github.com/speinnov/logmonitoring.git
 git push -u origin master
 
