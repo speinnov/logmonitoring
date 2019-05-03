66cc20e611bf65b23d59e9c449b92968c5ff06a4
khenouche abdelhak <akhenouche@spectrum-innovation.com>
2019-05-04 00:49:50 +0200

    Update deploy_to_nexus.sh

--- logginmanager/deploy_to_nexus.sh
+++ logginmanager/deploy_to_nexus.sh
@@ -1,7 +1,8 @@
-export oldVersion=1.1-SNAPSHOT
+
+#export oldVersion=1.4
 export newVersion=1.2-SNAPSHOT
-mvn versions:set -DnewVersion=$oldVersion 
-mvn clean install
+#mvn versions:set -DnewVersion=$oldVersion 
+#mvn clean install
 mvn versions:set -DnewVersion=$newVersion 
 mvn clean install
 
@@ -9,7 +10,11 @@ mvn clean install
 #mvn release:perform -P auth 
 #cd target/checkout 
 #mvn clean deploy
+mvn clean
+mvn install
 
-mvn deploy:deploy-file -DgroupId=com.speinnov.tools.monitoring -DartifactId=logginmanager -Dversion=$newVersion -DgeneratePom=true -Dpackaging=jar -DrepositoryId=talend_snapshots  -Durl=http://vps40346.lws-hosting.com:8888/nexus/content/repositories/talend_snapshots -Dfile=target/logginmanager-$newVersion.jar
+#mvn deploy:deploy-file -DgroupId=com.speinnov.tools.monitoring -DartifactId=logginmanager -Dversion=$newVersion -DgeneratePom=true -Dpackaging=jar -DrepositoryId=talend_snapshots  -Durl=http://vps40346.lws-hosting.com:8888/nexus/content/repositories/talend_snapshots -Dfile=target/logginmanager-$newVersion.jar
+
+mvn deploy:deploy-file  -DgeneratePom=false  -DrepositoryId=talend-snapshots  -Durl=http://vps40346.lws-hosting.com:8888/nexus/content/repositories/talend_snapshots   -DpomFile=pom.xml  -Dfile=target/logginmanager-1.2-SNAPSHOT.jar
 
 
